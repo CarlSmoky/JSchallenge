6e3b812ba928fca7593efd78cd32212644a42329
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-06 16:51:14 -0400

    Add logestString.js

---
+++ array/logestString.js
@@ -0,0 +1,15 @@
+// Write a function that takes an array of strings as argument
+// Return the longest string
+
+const myFunction = arr => {
+  let longestStr = "";
+  arr.forEach(element => {
+    if (longestStr.length < element.length) {
+      longestStr = element;
+    }
+  });
+  return longestStr;
+};
+
+console.log(myFunction(['help', 'me'])); //'help'
+console.log(myFunction(['I', 'need', 'candy'])); //'candy'
