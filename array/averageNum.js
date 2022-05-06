55b42edbbd0b0686f9c3ca481a415fa4edf6e878
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-06 16:11:23 -0400

    Refactor

--- array/averageNum.js
+++ array/averageNum.js
@@ -1,14 +1,20 @@
 // Write a function that takes an array of numbers as argument
 // It should return the average of the numbers
 
-const myFunction = arr => {
-  let sum = 0;
-  arr.forEach(element => {
-    sum += element;
-  });
-  return sum / arr.length;
-};
+// const myFunction = arr => {
+//   let sum = 0;
+//   arr.forEach(element => {
+//     sum += element;
+//   });
+//   return sum / arr.length;
+// };
 
+//Refactor
+const numbers = [1,2,3,4,5,6,7,8];
+
+const myFunction = arr => {
+  return arr.reduce((accumulator, current) => accumulator + current, 0) / arr.length;
+};
 
 console.log(myFunction([10, 100, 40])); //50
 console.log(myFunction([10, 100, 1000])); //370
