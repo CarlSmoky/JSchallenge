6f023af444a1d27a1c46fb6066f9110c52f8fa8a
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-06 17:18:18 -0400

    Refacor

--- array/allSameElement.js
+++ array/allSameElement.js
@@ -2,9 +2,25 @@
 // It should return true if all elements in the array are equal
 // It should return false otherwise
 
+// const myFunction = arr => {
+//   const str = arr.reduce((a, b) => a === b ? a : b);
+//   if (str === arr[0]) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// Refactor1
 const myFunction = arr => {
   return arr.reduce((a, b) => a === b ? a : b) === arr[0];
 };
+
+//// Refactor2
+// const myFunction = arr => {
+//   return new Set(arr).size === 1;
+// };
+
 
 console.log(myFunction([true, true, true, true]));
 console.log(myFunction(['test', 'test', 'test']));
