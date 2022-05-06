76323e1280179e3f18cbb286cbde4c264c0208ff
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-06 15:51:45 -0400

    Add array/numOfElement.js

---
+++ array/numOfElement.js
@@ -0,0 +1,8 @@
+// Write a function that takes an array (a) as argument
+// Return the number of elements in a
+
+const myFunction = a => {
+  return a.length;
+};
+
+console.log(myFunction([1,2,2,4]));
