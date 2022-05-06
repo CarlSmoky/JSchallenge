67aa8b785a749e80d26185cfcca21f0346ba1f8c
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-06 16:56:39 -0400

    Refactor

--- array/logestString.js
+++ array/logestString.js
@@ -1,14 +1,19 @@
 // Write a function that takes an array of strings as argument
 // Return the longest string
 
+// const myFunction = arr => {
+//   let longestStr = "";
+//   arr.forEach(element => {
+//     if (longestStr.length < element.length) {
+//       longestStr = element;
+//     }
+//   });
+//   return longestStr;
+// };
+
+//Refactor
 const myFunction = arr => {
-  let longestStr = "";
-  arr.forEach(element => {
-    if (longestStr.length < element.length) {
-      longestStr = element;
-    }
-  });
-  return longestStr;
+  return arr.reduce((a, b) => a.length <= b.length ? b : a);
 };
 
 console.log(myFunction(['help', 'me'])); //'help'
