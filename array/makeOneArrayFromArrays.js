ad65ea3f52941dbb86ad40de99da878fc1663a2f
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-06 17:35:01 -0400

    Refactor

--- array/makeOneArrayFromArrays.js
+++ array/makeOneArrayFromArrays.js
@@ -1,14 +1,19 @@
 // Write a function that takes arguments an arbitrary number of arrays
 // It should return an array containing the values of all arrays
 
+// const myFunction = (...arrays) => {
+//   let newArr = [];
+//   arrays.forEach(array => {
+//     array.forEach(el => {
+//       newArr.push(el);
+//     });
+//   });
+//   return newArr;
+// };
+
+// Refactor
 const myFunction = (...arrays) => {
-  let newArr = [];
-  arrays.forEach(array => {
-    array.forEach(el => {
-      newArr.push(el);
-    });
-  });
-  return newArr;
+  return arrays.flat();
 };
 
 console.log(myFunction([1, 2, 3], [4, 5, 6])); //[1, 2, 3, 4, 5, 6]
