0b1fa392610e97ac2e46e7460cd38ca1324f68ef
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-06 15:44:59 -0400

    Add removeSpecificElement.js

---
+++ array/removeSpecificElement.js
@@ -0,0 +1,12 @@
+// Write a function that takes an array (a) and a value (b) as argument
+// The function should clean a from all occurrences of b
+// Return the filtered array
+
+const myFunction = (a, n) => {
+  return a.filter(e => e !== n);
+};
+
+console.log(myFunction([1,2,3], 2)); //[1, 3]
+console.log(myFunction([1,2,'2'], '2')); //[1, 2]
+console.log(myFunction([false,'2',1], false)); //['2', 1]
+console.log(myFunction([1,2,'2',1], 1));//[2, '2']
