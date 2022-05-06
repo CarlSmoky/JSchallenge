e0673f3051bf88d58bb094742c6402b8de5947ba
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-06 17:34:06 -0400

    Add makeOneArrayFromArrays.js

---
+++ array/makeOneArrayFromArrays.js
@@ -0,0 +1,17 @@
+// Write a function that takes arguments an arbitrary number of arrays
+// It should return an array containing the values of all arrays
+
+const myFunction = (...arrays) => {
+  let newArr = [];
+  arrays.forEach(array => {
+    array.forEach(el => {
+      newArr.push(el);
+    });
+  });
+  return newArr;
+};
+
+console.log(myFunction([1, 2, 3], [4, 5, 6])); //[1, 2, 3, 4, 5, 6]
+console.log(myFunction(['a', 'b', 'c'], [4, 5, 6])); //['a', 'b', 'c', 4, 5, 6]
+console.log(myFunction([true, true], [1, 2], ['a', 'b'])
+); //[true, true, 1, 2, 'a', 'b']
