d8a8a7673c074c38e1a5b92b09a6206ac67b7e16
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-08 08:47:18 -0400

    Add array/concatenateArraySort.js

---
+++ array/concatenateArraySort.js
@@ -0,0 +1,20 @@
+// Write a function that takes two arrays as arguments
+// Merge both arrays and remove duplicate values
+// Sort the merge result in ascending order
+// Return the resulting array
+
+const myFunction = (a, b) => {
+  // return a.concat(b);
+  // return [].concat(a, b);
+  let concatArr = [...a, ...b];
+  let newArr = [];
+  concatArr.filter(el => {
+    if (!newArr.includes(el)) {
+      newArr.push(el);
+    };
+  })
+  return newArr.sort((a, b) => a -b);
+};
+
+console.log(myFunction([1, 2, 3], [3, 4, 5]));
+console.log(myFunction([-10, 22, 333, 42], [-11, 5, 22, 41, 42]));
