b65bc735c90e5af2c82b033c8bad36cdd500dcec
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-08 08:48:07 -0400

    Refactor concatenateArraySort.js

--- array/concatenateArraySort.js
+++ array/concatenateArraySort.js
@@ -16,5 +16,11 @@ const myFunction = (a, b) => {
   return newArr.sort((a, b) => a -b);
 };
 
+// Refactor
+// Set objects that store unique values of any type, whether primitive values or object references.
+// const myFunction = (a, b) => {
+//   return [...new Set([...a, ...b])].sort((x, y) => x - y);
+// };
+
 console.log(myFunction([1, 2, 3], [3, 4, 5]));
 console.log(myFunction([-10, 22, 333, 42], [-11, 5, 22, 41, 42]));
