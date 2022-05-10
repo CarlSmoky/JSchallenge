70d3cc12b08cc97bc96257d89ac1e1287c6f6a1b
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-10 08:01:37 -0400

    Refactor

--- array/repeatNthInArrray.js
+++ array/repeatNthInArrray.js
@@ -2,13 +2,25 @@
 // Save every nth element in a new array
 // Return the new array
 
-const myFunction = (a, n) => {
-let result = [];
-for (let i = 0; i < a.length; i++) {
-  (i + 1) % n === 0 ? result.push(a[i]) : '';
+// const myFunction = (a, n) => {
+// let result = [];
+// for (let i = 0; i < a.length; i++) {
+//   (i + 1) % n === 0 ? result.push(a[i]) : '';
+// }
+// return result;
+// };
+
+// Refactor
+function myFunction(a, n) {
+  let rest = [...a];
+  let result = [];
+  for (let i = 0; i < a.length; i++) {
+     if (rest.length < n) break;
+     result.push(rest[n - 1]);
+     rest = rest.slice(n);
+  }
+  return result;
 }
-return result;
-};
 
 console.log(myFunction([1,2,3,4,5,6,7,8,9,10],3)); //[3,6,9]
 console.log(myFunction([10,9,8,7,6,5,4,3,2,1],5)); //[6,1]
