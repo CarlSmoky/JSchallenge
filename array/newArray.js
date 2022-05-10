e51f6562b1a4e3a67cc809880874abd3b903a919
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-10 07:42:02 -0400

    Add newArray.js

---
+++ array/newArray.js
@@ -0,0 +1,12 @@
+// Write a function that takes an array with arbitrary elements and a number as arguments
+// Return a new array, the first element should be either the given number itself
+// or zero if the number is smaller than 6
+// The other elements should be the elements of the original array
+// Try not to mutate the original array
+const myFunction =(arr, num) => {
+  const bundary = 6;
+  num >= bundary ? arr.unshift(num) : arr.unshift(0);
+  return arr;
+};
+console.log(myFunction([1,2,3], 6)); //[6,1,2,3]
+console.log(myFunction(['a','b'], 2)); //[0,'a','b']
