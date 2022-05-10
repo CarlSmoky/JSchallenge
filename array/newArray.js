2c34b1d89df352d4beb1c6a60d97c3417e63f0bb
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-10 07:43:25 -0400

    Refactor

--- array/newArray.js
+++ array/newArray.js
@@ -3,10 +3,15 @@
 // or zero if the number is smaller than 6
 // The other elements should be the elements of the original array
 // Try not to mutate the original array
-const myFunction =(arr, num) => {
-  const bundary = 6;
-  num >= bundary ? arr.unshift(num) : arr.unshift(0);
-  return arr;
-};
+// const myFunction =(arr, num) => {
+//   const bundary = 6;
+//   num >= bundary ? arr.unshift(num) : arr.unshift(0);
+//   return arr;
+// };
+
+//Refactor
+const myFunction = (arr, num) => {
+  return [...(num > 5 ? [num] : [0]), ...arr];
+}
 console.log(myFunction([1,2,3], 6)); //[6,1,2,3]
 console.log(myFunction(['a','b'], 2)); //[0,'a','b']
