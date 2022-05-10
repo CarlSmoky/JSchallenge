44bc9d6c6d1341abd9d876c1081e2de8acdabe1a
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-10 08:08:17 -0400

    Add accessPropertyByArbitrarykey.js

---
+++ object/accessPropertyByArbitrarykey.js
@@ -0,0 +1,13 @@
+// Write a function that takes an object with two properties and a string as arguments
+// It should return the value of the property with key equal to the value of the string
+
+const myFunction = (obj, key) => {
+  return obj[key];
+};
+
+console.log(myFunction({  continent: 'Asia',  country: 'Japan'}, 'continent'));
+// Expected
+// 'Asia'
+console.log(myFunction({  country: 'Sweden',  continent: 'Europe'}, 'country'));
+// Expected
+// 'Sweden'
