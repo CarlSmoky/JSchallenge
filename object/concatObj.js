4c9e018f8513a10718395b60576629fa2e3e47d3
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-12 11:25:17 -0400

    Refactor

--- object/concatObj.js
+++ object/concatObj.js
@@ -5,11 +5,17 @@
 // Return the resulting object
 // It should have the properties 'a', 'b', 'c', 'd', and 'e'
 
+// const myFunction = (x, y) => {
+//   const {b, ...rest} = y;
+//   const newObj = {};
+//   newObj.d = b;
+//   return {...x, ...rest, ...newObj};
+// };
+
+//Refactor
 const myFunction = (x, y) => {
-  const {b, ...rest} = y;
-  const newObj = {};
-  newObj.d = b;
-  return {...x, ...rest, ...newObj};
-};
+  const { b, ...rest } = y;
+  return { ...x, ...rest, d: b };
+}
 
 console.log(myFunction({ a: 1, b: 2 }, { c: 3, b: 4, e: 5 })); //{ a: 1, b: 2, c: 3, e: 5, d: 4}
