13ec7876ec8184f3fd6b7ba8c197e10f52ee7137
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-12 09:34:46 -0400

    Refactor

--- object/removeSpecificObj.js
+++ object/removeSpecificObj.js
@@ -2,14 +2,20 @@
 // It should return an object with all original object properties
 // except for the property with key 'b'
 
+// const myFunction = obj => {
+//   let result = {};
+//   for (let el in obj) {
+//     if (el !== 'b') {
+//       result[el] =obj[el] ;
+//     }
+//   }
+//   return result;
+// };
+
+//Refactor
 const myFunction = obj => {
-  let result = {};
-  for (let el in obj) {
-    if (el !== 'b') {
-      result[el] =obj[el] ;
-    }
-  }
-  return result;
-};
+  const { b, ...rest } = obj;
+  return rest;
+}
 
 console.log(myFunction({ a: 1, b: 7, c: 3 })); //{ a: 1, c: 3 }
