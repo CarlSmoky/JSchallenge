4bd78f45d767ece0da85d3c589dae2af9415639b
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-13 10:42:33 -0400

    Add addObject.js

---
+++ object/addObject.js
@@ -0,0 +1,13 @@
+// Write a function that takes an array of objects and a string as arguments
+// Add a property with key 'continent' and value equal to the string to each of the objects
+// Return the new array of objects
+// Tipp: try not to mutate the original array
+
+const myFunction = (arr, str) => {
+  return arr.map(obj => {  
+     return {...obj, 'continent': str};
+  })
+};
+
+console.log(myFunction([{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia')); 
+//[{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
