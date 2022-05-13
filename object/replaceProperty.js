367c8f925dad3d386fc531b722d1007bea769196
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-13 11:56:26 -0400

    Refactor

--- object/replaceProperty.js
+++ object/replaceProperty.js
@@ -3,14 +3,23 @@
 // Replace empty strings and strings that contain only whitespace with null values
 // Return the resulting object
 
+// const myFunction = obj => {
+//   Object.entries(obj).forEach((arr) => {
+//     if (!arr[1].trim()) {
+//       obj[arr[0]] = null;
+//     }
+//   });
+//   return obj;
+// };
+
+// Refactor
 const myFunction = obj => {
-  Object.entries(obj).forEach((arr) => {
-    if (!arr[1].trim()) {
-      obj[arr[0]] = null;
-    }
-  });
-  return obj;
-};
+  const newObj = { ...obj };
+  for (key in newObj) {
+    if (newObj[key].trim() === '') newObj[key] = null;
+  }
+  return newObj;
+}
 
 console.log(myFunction({ a: 'a', b: 'b', c: '' })); //{ a: 'a', b: 'b', c: null }
 console.log(myFunction({ a: '', b: 'b', c: ' ', d: 'd' })); //{ a: null, b: 'b', c: null, d: 'd' }
