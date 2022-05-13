158d89fa9e0840f18fc19175b3bdfccb31983968
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-13 12:59:30 -0400

    Refactor object/multipleByArgument.js

--- object/multipleByArgument.js
+++ object/multipleByArgument.js
@@ -2,12 +2,19 @@
 // Multiply all values of 'a' by 'b'
 // Return the resulting object
 
-const myFunction = (a, b) => {
-  const newObj = {...a};
-  Object.keys(newObj).forEach(el => {
-    newObj[el] = newObj[el] * b;
-  });
-  return newObj;
-};
+// const myFunction = (a, b) => {
+//   const newObj = {...a};
+//   Object.keys(newObj).forEach(el => {
+//     newObj[el] = newObj[el] * b;
+//   });
+//   return newObj;
+// };
 
-console.log(myFunction({a:1,b:2,c:3},3)); //{a:3,b:6,c:9}
+// Refactor
+
+const myFunction = (a, b) => {
+  return Object.entries(a).reduce((acc, [key, val]) => {
+    return { ...acc, [key]: val * b };
+  }, {});
+}
+console.log(myFunction({ a: 1, b: 2, c: 3 }, 3)); //{a:3,b:6,c:9}
