f9bb493ec70b450bdcfd7937110ef1f6cbabf1e0
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-13 13:48:45 -0400

    Refactor countSameProperty.js

--- object/countSameProperty.js
+++ object/countSameProperty.js
@@ -3,17 +3,25 @@
 // It should have a key for each unique value of the array
 // The corresponding object value should be the number of times the key occurs within the array
 
-const myFunction = (a) => {
-  const newObj = new Object;
-  a.forEach(el => {
-    if (!newObj[el]) {
-      newObj[el] = 1;
-    } else {
-      newObj[el] += 1;
-    }
-  })
-  return newObj;
-};
+// const myFunction = (a) => {
+//   const newObj = new Object;
+//   a.forEach(el => {
+//     if (!newObj[el]) {
+//       newObj[el] = 1;
+//     } else {
+//       newObj[el] += 1;
+//     }
+//   })
+//   return newObj;
+// };
+
+//Refactor
+
+const myFunction = a => {
+  return a.reduce((acc, cur) => {
+    return { ...acc, [cur]: (acc[cur] || 0) + 1 };
+  }, {});
+}
 
 console.log(myFunction([1,2,2,3])); //{1:1,2:2,3:1}
 console.log(myFunction([9,9,9,99])); //{9:3,99:1}
