010d60dedb48e7568904f97faa0cf4b6a902e988
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-14 10:39:57 -0400

    Refactor

--- dates/sameDate.js
+++ dates/sameDate.js
@@ -2,9 +2,16 @@
 // It should return true if they fall on the exact same day
 // It should return false otherwise
 
+// const myFunction = (a, b) => {
+//   return a.getTime() === b.getTime();
+// };
+
 const myFunction = (a, b) => {
-  return a.getTime() === b.getTime();
-};
+  return a.getFullYear() === b.getFullYear() &&
+         a.getMonth() === b.getMonth() &&
+         a.getDate()=== b.getDate();
+}
+
 
 console.log(myFunction(new Date('2000/01/01'), new Date('2000/01/01'))
 ); //true
