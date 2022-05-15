72dc130108d8035eaf9b7cca053bb61dc095b1e6
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-15 07:39:01 -0400

    Add diffTime.js

---
+++ dates/diffTime.js
@@ -0,0 +1,14 @@
+// Write a function that takes two date instances as argument
+// It should return true if the difference between the dates is less than or equal to 1 hour
+// It should return false otherwise
+
+const myFunction = (a, b) => {
+  const diffTime = Math.abs(a - b);
+  const diffHours = diffTime / (1000 * 60 * 60);
+  return (diffHours <= 1) ? true : false;
+};
+
+console.log(myFunction(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 08:45:00'))); //true
+console.log(myFunction(new Date('2000/01/01 09:00:00'), new Date('2000/01/01 08:45:00'))); //true
+console.log(myFunction(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 09:45:00'))) //false
+console.log(myFunction(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 09:00:00'))); //true
