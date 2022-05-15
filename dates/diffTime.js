959824932d227a9548f502ea926bc9f0b2142e43
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-15 07:41:00 -0400

    Refactor diffTime.js

--- dates/diffTime.js
+++ dates/diffTime.js
@@ -2,12 +2,17 @@
 // It should return true if the difference between the dates is less than or equal to 1 hour
 // It should return false otherwise
 
-const myFunction = (a, b) => {
-  const diffTime = Math.abs(a - b);
-  const diffHours = diffTime / (1000 * 60 * 60);
-  return (diffHours <= 1) ? true : false;
-};
+// const myFunction = (a, b) => {
+//   const diffTime = Math.abs(a - b);
+//   const diffHours = diffTime / (1000 * 60 * 60);
+//   return (diffHours <= 1) ? true : false;
+// };
 
+//Refactor
+const myFunction = (a, b) => {
+  return Math.abs(a - b) / 1000 / 60 <= 60;
+}
+ 
 console.log(myFunction(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 08:45:00'))); //true
 console.log(myFunction(new Date('2000/01/01 09:00:00'), new Date('2000/01/01 08:45:00'))); //true
 console.log(myFunction(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 09:45:00'))) //false
