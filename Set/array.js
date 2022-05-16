05d3e552fb72353717007864ccda1fcdcac116b3
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-16 07:51:00 -0400

    Add array.js

---
+++ Set/array.js
@@ -0,0 +1,15 @@
+// Write a function that takes a Set as argument
+// Convert the Set to an Array
+// Return the Array
+
+const myFunction = (set) => {
+  const myIterator = set.values();
+  let result = [];
+  for (const entry of myIterator) {
+    result.push(entry);
+  }
+  return result;
+}
+
+console.log(myFunction(new Set([1, 2, 3]))); //[1, 2, 3]
+console.log(myFunction(new Set([123]))); //[123]
