5fd764864dee124a70f541421c1431c5d96e6824
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-16 09:30:37 -0400

    Refactor contain.js

--- Set/contain.js
+++ Set/contain.js
@@ -2,16 +2,23 @@
 // Get the intersection of the sets
 // In other words, return a set containing all elements that are both in a as well as b
 
+// const myFunction = (a, b) => {
+//   const result = new Set();
+//   a.forEach(e => {
+//     b.forEach(item => {
+//       if (e === item) {
+//         result.add(item);
+//       }
+//     })
+//   })
+//   return result;
+// }
+
+//Refactor
 const myFunction = (a, b) => {
-  const result = new Set();
-  a.forEach(e => {
-    b.forEach(item => {
-      if (e === item) {
-        result.add(item);
-      }
-    })
-  })
-  return result;
+  const int = new Set();
+  a.forEach(el => b.has(el) && int.add(el));
+  return int;
 }
 
 console.log(myFunction(new Set([1, 2, 3]), new Set([4, 5, 6]))
