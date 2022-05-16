e6da45f8dd74edd52d4af28d3c126097c8235652
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-16 07:52:00 -0400

    Refactor array.js

--- Set/array.js
+++ Set/array.js
@@ -2,13 +2,18 @@
 // Convert the Set to an Array
 // Return the Array
 
-const myFunction = (set) => {
-  const myIterator = set.values();
-  let result = [];
-  for (const entry of myIterator) {
-    result.push(entry);
-  }
-  return result;
+// const myFunction = (set) => {
+//   const myIterator = set.values();
+//   let result = [];
+//   for (const entry of myIterator) {
+//     result.push(entry);
+//   }
+//   return result;
+// }
+
+//Refactor
+const myFunction = set => {
+  return [...set];
 }
 
 console.log(myFunction(new Set([1, 2, 3]))); //[1, 2, 3]
