e18fc86ecd67c14ce906313ad64295e079b12ed0
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-20 15:31:34 -0400

    Add dom/mouseenter/

---
+++ dom/mouseenter/app.js
@@ -0,0 +1,17 @@
+// Cursor enters and leaves element
+// In this scenario we want the color of the circle to change depending on the type of cursor movement. Use the function toggleColor to turn the circle orange when the cursor moves onto it. Reuse the same function to turn it black when the cursor leaves it.
+// The tricky part is that you have to call toggleColor with different values for the parameter isEntering. Verify that your code is working by hovering the circle with the mouse cursor and leaving it again.
+
+const element = document.getElementById("element");
+
+const toggleColor = (inEnterting) => {
+  element.style.background = inEnterting ? 'green' : 'red';
+}
+
+element.addEventListener('mouseenter', (e) => {
+  toggleColor(true);
+});
+
+element.addEventListener('mouseleave', (e) => {
+  toggleColor(false);
+});
