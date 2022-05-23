ea1dba69376ce934a4a04a67c68824c1596638c6
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-23 09:46:46 -0400

    Refactor dom/count/index.html

--- dom/count/app.js
+++ dom/count/app.js
@@ -2,10 +2,22 @@
 // Confirm your code by clicking the button!
 
 const button = document.getElementById("button");
-console.log(button);
+// console.log(button);
 
-button.addEventListener('click', (e) => {
-  const currentNum = Number(e.target.innerText);
-  return e.target.innerText = currentNum + 1;
-})
+// button.addEventListener('click', (e) => {
+//   const currentNum = Number(e.target.innerText);
+//   return e.target.innerText = currentNum + 1;
+// })
+
+//Refactor1 Not using event.target
+// button.addEventListener('click', () => {
+//   const currentNum = Number(button.innerText);
+//   return button.innerText = currentNum + 1;
+// })
+
+//Refactor2 ParseInt
+button.addEventListener('click', () => {
+  const oldValue = parseInt(button.innerText, 10);
+  button.innerText = oldValue + 1;
+});
 
