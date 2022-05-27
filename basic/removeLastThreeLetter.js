1f75b0575c73b0171e6c0e665f470d34eda9acbc
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-27 14:13:41 -0400

    Add basic/removeLastThreeLetter.js

---
+++ basic/removeLastThreeLetter.js
@@ -0,0 +1,10 @@
+// Write a function that takes a string (a) as argument
+// Remove the last 3 characters of a
+// Return the result
+
+const myFunction = (a) => {
+  return a.slice(0, a.length - 3);
+  return a.slice(0, - 3);
+};
+
+console.log(myFunction('abcdefg')); //'abcd'
