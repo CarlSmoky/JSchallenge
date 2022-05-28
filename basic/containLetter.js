7532665db0b3436ab05b86f52e5b36e097f9aac4
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-28 07:04:50 -0400

    Refactor

--- basic/containLetter.js
+++ basic/containLetter.js
@@ -1,13 +1,18 @@
 // Write a function that takes two strings (a and b) as arguments
 // Return the number of times a occurs in b
 
+// const myFunction = (a, b) => {
+//   const strArr = b.split('');
+//   let countLetter = 0;
+//   strArr.forEach(letter => {
+//     if (letter === a) countLetter += 1;
+//   })
+//   return countLetter;
+// };
+
+//Refactor
 const myFunction = (a, b) => {
-  const strArr = b.split('');
-  let countLetter = 0;
-  strArr.forEach(letter => {
-    if (letter === a) countLetter += 1;
-  })
-  return countLetter;
+  return b.split(a).length - 1;
 };
 
 console.log(myFunction('m', 'how many times does the character occur in this sentence?')); //2
