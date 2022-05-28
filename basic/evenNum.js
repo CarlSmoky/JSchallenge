d16f2e821c00b631b786708e22171a3aebb2af30
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-28 07:03:41 -0400

    Add basic/evenNum.js

---
+++ basic/evenNum.js
@@ -0,0 +1,10 @@
+// Write a function that takes a number as argument
+// If the number is even, return true
+// Otherwise, return false
+
+const myFunction = (a) => {
+  return a % 2 === 0;
+};
+
+console.log(myFunction(10)); //true
+console.log(myFunction(-111)); //false
