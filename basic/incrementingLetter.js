22c1fd6b1bc96995f28e792b7f46466758f4c449
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-30 08:32:35 -0400

    Refactor

--- basic/incrementingLetter.js
+++ basic/incrementingLetter.js
@@ -3,14 +3,21 @@
 // Increment each letter to the next letter in the alphabet
 // Return the correct word
 
-const myFunction = (str) => {
-  const strArr = str.split('');
-  const charCodes = strArr.map((el) => el.charCodeAt(0));
-  let newStrArr = [];
-  charCodes.forEach((code) => {
-    newStrArr.push(String.fromCharCode(code + 1));
-  })
-  return newStrArr.join('');
+// const myFunction = (str) => {
+//   const strArr = str.split('');
+//   const charCodes = strArr.map((el) => el.charCodeAt(0));
+//   let newStrArr = [];
+//   charCodes.forEach((code) => {
+//     newStrArr.push(String.fromCharCode(code + 1));
+//   })
+//   return newStrArr.join('');
+// }
+
+// Refactor
+const myFunction = () => {
+  const arr = [...str];
+  const correctedArray = arr.map(e => String.fromCharCode(e.charCodeAt()+1));
+  return correctedArray.join('');
 }
 
 console.log(myFunction('bnchmf')); //'coding'
