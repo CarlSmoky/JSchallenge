6389790527ae3c7d4c7afdaab81a77f40599b447
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-31 09:56:54 -0400

    Add basic/splitNum.js

---
+++ basic/splitNum.js
@@ -0,0 +1,14 @@
+// Write a function that takes a number (a) as argument
+// Split a into its individual digits and return them in an array
+// Tipp: you might want to change the type of the number for the splitting
+
+const myFunction = (a) => {
+  const string = a.toString();
+  let arr = [];
+  for (let i = 0; i < string.length; i++) {
+    arr.push(Number(string[i]));
+  }
+  return arr;
+}
+
+console.log(myFunction(10)); //[1, 0]
