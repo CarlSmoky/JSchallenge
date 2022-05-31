8d7c1c5f31259b25235ef9a5db9e452f802865ee
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-31 09:57:57 -0400

    Refactor

--- basic/splitNum.js
+++ basic/splitNum.js
@@ -2,13 +2,20 @@
 // Split a into its individual digits and return them in an array
 // Tipp: you might want to change the type of the number for the splitting
 
+// const myFunction = (a) => {
+//   const string = a.toString();
+//   let arr = [];
+//   for (let i = 0; i < string.length; i++) {
+//     arr.push(Number(string[i]));
+//   }
+//   return arr;
+// }
+
+//Refactor
 const myFunction = (a) => {
-  const string = a.toString();
-  let arr = [];
-  for (let i = 0; i < string.length; i++) {
-    arr.push(Number(string[i]));
-  }
-  return arr;
+  const string = a + '';
+  const strings = string.split('');
+  return strings.map(digit => Number(digit))
 }
 
 console.log(myFunction(10)); //[1, 0]
