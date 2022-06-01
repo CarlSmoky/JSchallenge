94747cfc4499c459e73ce3d925076c6ee60cc8c5
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-01 10:25:55 -0400

    Refactor

--- basic/cleanUp.js
+++ basic/cleanUp.js
@@ -4,21 +4,29 @@
 // 'Javascript', 'Countryside', and 'Downtown'
 // You might want to apply basic JS string methods such as replace(), split(), slice() etc
 
+// const myFunction = (a, b) => {
+//   const isLetter = (str) => {
+//     return str.match(/[a-z]/i) ? true : false;
+//   }
+//   let newString = '';
+//   a.split('').forEach((str, i) => {
+//     if (i === 0) {
+//       str = str.toUpperCase();
+//     }
+//     newString += isLetter(str) ? str : '';
+//   })
+//   for (let i = b.length - 1; i >= 0; i--) {
+//     newString += isLetter(b[i]) ? b[i] : '';
+//   }
+//   return newString;
+// }
+
+//Refactor
 const myFunction = (a, b) => {
-  const isLetter = (str) => {
-    return str.match(/[a-z]/i) ? true : false;
-  }
-  let newString = '';
-  a.split('').forEach((str, i) => {
-    if (i === 0) {
-      str = str.toUpperCase();
-    }
-    newString += isLetter(str) ? str : '';
-  })
-  for (let i = b.length - 1; i >= 0; i--) {
-    newString += isLetter(b[i]) ? b[i] : '';
-  }
-  return newString;
+  const func = x => x.replace('%','');
+  const first = func(a);
+  const second = func(b).split('').reverse().join('');
+  return first.charAt(0).toUpperCase() + first.slice(1) + second;
 }
 
 console.log(myFunction('java', 'tpi%rcs')); //'Javascript'
