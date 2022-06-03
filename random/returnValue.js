d26e3a7c472a501ebc07945390ae0e2995d7ec44
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-03 08:28:32 -0400

    Add random/returnValue.js

---
+++ random/returnValue.js
@@ -0,0 +1,16 @@
+// Write a function that takes an object as argument
+// Some of the property values contain empty strings
+// Replace empty strings and strings that contain only whitespace with null values
+// Return the resulting object
+
+const myFunction = (obj) => {
+  let newObj = new Object;
+   Object.entries(obj).forEach(([key, value]) => {
+    newObj[key] = (value.trim() === '') ? null : value;
+  })
+  return newObj;
+}
+
+console.log(myFunction({ a: 'a', b: 'b', c: '' })); //{ a: 'a', b: 'b', c: null }
+console.log(myFunction({ a: '', b: 'b', c: ' ', d: 'd' })); //{ a: null, b: 'b', c: null, d: 'd' }
+console.log(myFunction({ a: 'a', b: 'b ', c: ' ', d: '' })); //{ a: 'a', b: 'b ', c: null, d: null }
