8995cc6f9fda92b06d900074fc7358ad936efcb2
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-04 10:06:28 -0400

    Refactor

--- random/mergeArray.js
+++ random/mergeArray.js
@@ -3,18 +3,23 @@
 // Sort the merge result in ascending order
 // Return the resulting array
 
+// const myFunction = (a, b) => {
+//   const mySet = new Set();
+//   const concatArrays = [...a, ...b];
+//   concatArrays.forEach((item) => {
+//     mySet.add(item);
+//   })
+//   let arr = [];
+//   for (let item of mySet.values()) arr.push(item);
+//   arr = arr.sort((a, b) => {
+//     return a - b;
+//   })
+//   return arr;
+// }
+
+//Refactor
 const myFunction = (a, b) => {
-  const mySet = new Set();
-  const concatArrays = [...a, ...b];
-  concatArrays.forEach((item) => {
-    mySet.add(item);
-  })
-  let arr = [];
-  for (let item of mySet.values()) arr.push(item);
-  arr = arr.sort((a, b) => {
-    return a - b;
-  })
-  return arr;
+  return [...new. Set([...a, ...b])].sort((x, y) => x - y);
 }
 
 console.log(myFunction([1, 2, 3], [3, 4, 5])); //[ 1, 2, 3, 4, 5 ]
