d6fb91432fd8c255c685454cf6c4e92e482e39b7
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-05 10:41:49 -0400

    Refactor

--- random/convertNumToArray.js
+++ random/convertNumToArray.js
@@ -2,12 +2,19 @@
 // Split a into its individual digits and return them in an array
 // Tipp: you might want to change the type of the number for the 
 
+// const myFunction = (a) => {
+//   let result = [];
+//   for (let str of a.toString()) {
+//     result.push(Number(str));
+//   }
+//   return result;
+// }
+
+//Refactor
 const myFunction = (a) => {
-  let result = [];
-  for (let str of a.toString()) {
-    result.push(Number(str));
-  }
-  return result;
+  const string = a + '';
+  const strings = string.split('');
+  return strings.map(digit => Number(digit))
 }
 
 console.log(myFunction(10)); //[1,0]
