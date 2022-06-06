6394327c3231c17b9e69e9d4c19ad4951d5b7804
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-06 11:56:25 -0400

    Add random/checkTime.js

---
+++ random/checkTime.js
@@ -0,0 +1,10 @@
+// Write a function that takes two date instances (a and b) as arguments
+// It should return true if a is earlier than b
+// It should return false otherwise
+
+const myFunction = (a, b) => {
+  return a.getTime() < b.getTime();
+}
+
+console.log(myFunction(new Date('2000/01/01 08:00:00'), new Date('2000/01/01 08:45:00'))); //true
+console.log(myFunction(new Date('2000/01/01 08:45:00'), new Date('2000/01/01 08:00:00'))) //false);
