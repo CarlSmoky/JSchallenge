273b62b2023aaad612fd0b36d6b7ec87a26dc848
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-06 12:21:16 -0400

    Refactor createObj.js

--- random/createObj.js
+++ random/createObj.js
@@ -3,16 +3,23 @@
 // It should have a key for each unique value of the array
 // The corresponding object value should be the number of times the key occurs within the array
 
+// const myFunction = (a) => {
+//   const obj = new Object;
+//   a.forEach(el => {
+//     if (obj[el]) {
+//       obj[el] ++ ;
+//     } else {
+//       obj[el] = 1;
+//     }
+//   })
+//   return obj;
+// }
+
+//Refactor
 const myFunction = (a) => {
-  const obj = new Object;
-  a.forEach(el => {
-    if (obj[el]) {
-      obj[el] ++ ;
-    } else {
-      obj[el] = 1;
-    }
-  })
-  return obj;
+  return a.reduce((acc, cur) => {
+    return { ...acc, [cur]: (acc[cur] || 0) + 1 };
+  }, {});
 }
 
 console.log(myFunction([1,2,2,3])); //{1:1,2:2,3:1}
