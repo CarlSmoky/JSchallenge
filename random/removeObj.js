8635812f8f8b5be8b593ea881a25c621613d2858
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-08 08:38:38 -0400

    Refactor2

--- random/removeObj.js
+++ random/removeObj.js
@@ -15,17 +15,21 @@
 // }
 
 //Refactor1
-const myFunction = obj => {
-  const newObj = new Object;
-  const arr = Object.entries(obj)
-    .filter(([key, value]) => key !== 'b')
-  arr.forEach(el => {
-    newObj[el[0]] = el[1];
-  })
-  return newObj;
+// const myFunction = obj => {
+//   const newObj = new Object;
+//   const arr = Object.entries(obj)
+//     .filter(([key, value]) => key !== 'b')
+//   arr.forEach(el => {
+//     newObj[el[0]] = el[1];
+//   })
+//   return newObj;
+// }
+
+//Refactor2
+const myFunction = (obj) => {
+  const { b, ...rest } = obj;
+  return rest;
 }
-
-
 
 console.log(myFunction({ a: 1, b: 7, c: 3 })); //{ a: 1, c: 3 }
 console.log(myFunction({ b: 0, a: 7, d: 8 })); //{ a: 7, d: 8 }
