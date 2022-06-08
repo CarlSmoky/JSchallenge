749908f65e0d08685c8f5aab90a9d21783ca33ca
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-08 08:37:53 -0400

    Add random/removeObj.js

---
+++ random/removeObj.js
@@ -0,0 +1,21 @@
+// Write a function that takes an object as argument
+// It should return an object with all original object properties
+// except for the property with key 'b'
+
+const myFunction = obj => {
+  const newObj = new Object;
+  Object.entries(obj)
+    .forEach(([key, value]) => {
+      if (key !== 'b') {
+        newObj[key] = value;
+      }
+    })
+
+  return newObj;
+}
+
+
+
+console.log(myFunction({ a: 1, b: 7, c: 3 })); //{ a: 1, c: 3 }
+console.log(myFunction({ b: 0, a: 7, d: 8 })); //{ a: 7, d: 8 }
+console.log(myFunction({ e: 6, f: 4, b: 5, a: 3 })); //{ e: 6, f: 4, a: 3 }
