c44abb1a8c458fba88d49d29655e565ba3c90b16
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-10 07:58:32 -0400

    Refactor with reduce

--- random/longestStr.js
+++ random/longestStr.js
@@ -1,14 +1,19 @@
 // Write a function that takes an array of strings as argument
 // Return the longest string
 
+// const myFunction = (arr) => {
+//   let longestStr = '';
+//   arr.forEach((str) => {
+//     if (longestStr.length < str.length) {
+//       longestStr = str;
+//     }
+//   })
+//   return longestStr;
+// }
+
+//Refactor
 const myFunction = (arr) => {
-  let longestStr = '';
-  arr.forEach((str) => {
-    if (longestStr.length < str.length) {
-      longestStr = str;
-    }
-  })
-  return longestStr;
+  return arr.reduce((a, b) => a.length <= b.length ? b : a);
 }
 
 console.log(myFunction(['help', 'me'])); //help
