823d9018adee5166030ad9f7a57147bea4eb68d7
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-13 08:58:45 -0400

    Add random/countNegative.js

---
+++ random/countNegative.js
@@ -0,0 +1,10 @@
+// Write a function that takes an array of numbers as argument
+// Return the number of negative values in the array
+
+const myFunction = (a) => {
+  return a.filter(el => el < 0).length;
+}
+
+console.log(myFunction([1,-2,2,-4])); //2
+console.log(myFunction([0,9,1])); //0
+console.log(myFunction([4,-3,2,1,0])); //1
