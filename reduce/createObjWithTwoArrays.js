37147619a51cdbdb923fe8149113f37216eb269c
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-18 09:27:34 -0600

    Add reduce/createObjWithTwoArrays.js

---
+++ reduce/createObjWithTwoArrays.js
@@ -0,0 +1,12 @@
+// Write a function that takes two arrays (a and b) as arguments
+// Create an object that has properties with keys 'a' and corresponding values 'b'
+// Return the object
+
+
+const myFunction = (a, b) => {
+  return a.reduce((acc, cur, i) => {
+    return {...acc, [cur]: b[i]} 
+  }, {})
+};
+
+console.log(myFunction(['a','b','c'],[1,2,3])); //{a:1,b:2,c:3}
