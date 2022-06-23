69f322590a4766ef98b8d81c3fbc6a088994ecee
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-23 13:24:06 -0400

    Refactor using filter

--- reduce/negativeNum.js
+++ reduce/negativeNum.js
@@ -1,11 +1,15 @@
 // Write a function that takes an array of numbers as argument
 // Return the number of negative values in the array
 
-const myFunction = a => {
-  return a.reduce((acc, cur) => {
-    return cur < 0 ? acc + 1 : acc + 0;
-  }, 0)
-};
+// const myFunction = a => {
+//   return a.reduce((acc, cur) => {
+//     return cur < 0 ? acc + 1 : acc + 0;
+//   }, 0)
+// };
 
+//Refactor
+const myFunction = a => {
+  return a.filter(e => e < 0).length;
+}
 console.log(myFunction([1,-2,2,-4])); //2
 console.log(myFunction([0,9,1])); //0
