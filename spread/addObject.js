c5e0bacfc97e9a2d8df8df415c6e7a07ba340059
tsumitakaoru <k.tsumita01@gmail.com>
2022-06-30 11:06:00 -0400

    Add spread/addObject.js

---
+++ spread/addObject.js
@@ -0,0 +1,21 @@
+// Write a function that takes an array of objects and a string as arguments
+// Add a property with key 'continent' and value equal to the string to each of the objects
+// Return the new array of objects
+// Tipp: try not to mutate the original array
+
+
+// const myFunction = (arr, str) => {
+//   const newArr = arr.map(obj => {
+//     return {...obj, continent: str};
+//   })
+//   return newArr;
+// }
+
+//Refactor
+const myFunction = (arr, str) => {
+  return arr.map(obj => ({...obj, continent: str}));
+}
+
+
+console.log(myFunction([{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'));
+//[{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
