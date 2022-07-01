a043bc93fb36fb68351ab5b68628fb35205175f2
tsumitakaoru <k.tsumita01@gmail.com>
2022-07-01 14:44:56 -0400

    Add spread/multiple.js

---
+++ spread/multiple.js
@@ -0,0 +1,11 @@
+// Write a function that takes an object (a) and a number (b) as arguments
+// Multiply all values of 'a' by 'b'
+// Return the resulting object
+
+
+const myFunction = (a, b) => {
+  return Object.entries(a).reduce((acc, [key, value]) => {
+    return {...acc, [key] : value * b}
+  }, {})
+}
+console.log(myFunction({ a: 1, b: 2, c: 3 }, 3)); //{a:3,b:6,c:9}
