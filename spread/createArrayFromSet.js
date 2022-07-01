6bbf156af91308973fbb0e66cfdd71663bc469d5
tsumitakaoru <k.tsumita01@gmail.com>
2022-07-01 14:36:22 -0400

    Add spread/createArrayFromSet.js

---
+++ spread/createArrayFromSet.js
@@ -0,0 +1,11 @@
+// Write a function that takes a Set as argument
+// Convert the Set to an Array
+// Return the Array
+
+
+const myFunction = set => {
+  return [...set];
+}
+
+console.log(myFunction(new Set([1, 2, 3]))); //[1, 2, 3]
+console.log(myFunction(new Set([123]))); //[123]
