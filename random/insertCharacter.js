8e60dd8d15100171d6e1c6a15d316c0bbdfd7aa9
tsumitakaoru <k.tsumita01@gmail.com>
2022-07-06 08:56:47 -0400

    Refactor

--- random/insertCharacter.js
+++ random/insertCharacter.js
@@ -2,20 +2,32 @@
 // Beginning at the end of 'a', insert 'b' after every 3rd character of 'a'
 // Return the resulting string
 
+// const myFunction = (a, b) => {
+//   let reversedArr = [...a].reverse();
+//   let newArr = [];
+//   for (let i = 1; i <= reversedArr.length; i++) {
+//     newArr.push(reversedArr[i - 1]);
+//     if (i % 3 === 0) {
+//       newArr.push(b);
+//     }
+//   }
+//   return newArr.reverse().join('');
+// }
+
+//Refactor
 const myFunction = (a, b) => {
   let reversedArr = [...a].reverse();
   let newArr = [];
   for (let i = 1; i <= reversedArr.length; i++) {
-    newArr.push(reversedArr[i - 1]);
+    newArr = [...newArr, reversedArr[i - 1]];
     if (i % 3 === 0) {
-      newArr.push(b);
+      newArr = [...newArr, b];
     }
   }
   return newArr.reverse().join('');
 }
 
 
-
-console.log(myFunction('1234567','.')); //'1.234.567'
-console.log(myFunction('abcde','$')); //'ab$cde'
-console.log(myFunction('zxyzxyzxyzxyzxyz','w')); //'zwxyzwxyzwxyzwxyzwxyz'
+console.log(myFunction('1234567', '.')); //'1.234.567'
+console.log(myFunction('abcde', '$')); //'ab$cde'
+console.log(myFunction('zxyzxyzxyzxyzxyz', 'w')); //'zwxyzwxyzwxyzwxyzwxyz'
