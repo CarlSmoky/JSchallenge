8e5f24e1e4b85d5cfc2ad04100a56c74f1a95cf2
tsumitakaoru <k.tsumita01@gmail.com>
2022-07-07 10:21:12 -0400

    Add random/decimal.js

---
+++ random/decimal.js
@@ -0,0 +1,11 @@
+// Write a function that takes a number (a) as argument
+// If a is a whole number (has no decimal place), return true
+// Otherwise, return false
+
+const myFunction = (a) => {
+  // return a - Math.floor(a) === 0;
+  return a % 1 === 0;
+}
+
+console.log(myFunction(4)); //true
+console.log(myFunction(1.123)); //false
