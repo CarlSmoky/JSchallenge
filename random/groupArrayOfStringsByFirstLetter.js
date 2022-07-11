0b35fda25f470a22d4bfb9406335a6d7afd30fb2
tsumitakaoru <k.tsumita01@gmail.com>
2022-07-11 13:09:47 -0400

    Refactor using reduce

--- random/groupArrayOfStringsByFirstLetter.js
+++ random/groupArrayOfStringsByFirstLetter.js
@@ -5,14 +5,22 @@
 // For example, the array ['Alf', 'Alice', 'Ben'] should be transformed to
 // { a: ['Alf', 'Alice'], b: ['Ben']}
 
-const myFunction = (arr) => {
-  const newObj = new Object;
-  arr.forEach(el => {
-    const arrays = newObj[el[0].toLowerCase()] ? newObj[el[0].toLowerCase()] : [];
-    arrays.push(el);
-    newObj[el[0].toLowerCase()] = arrays;
-  })
-  return newObj;
+// const myFunction = (arr) => {
+//   const newObj = new Object;
+//   arr.forEach(el => {
+//     const arrays = newObj[el[0].toLowerCase()] ? newObj[el[0].toLowerCase()] : [];
+//     arrays.push(el);
+//     newObj[el[0].toLowerCase()] = arrays;
+//   })
+//   return newObj;
+// }
+
+//Refactor
+const myFunction = arr => {
+  return arr.reduce((acc, cur) => {
+    const key = cur[0].toLowerCase();
+    return {...acc, [key] : [...(acc[key] || []), cur]}
+  }, {})
 }
 
 console.log(myFunction(['Alf', 'Alice', 'Ben'])); //{ a: ['Alf', 'Alice'], b: ['Ben']}
