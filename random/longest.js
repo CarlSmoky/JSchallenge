cb7ac99f4420a941a682ea0bf89df910230465fd
tsumitakaoru <k.tsumita01@gmail.com>
2022-07-19 17:07:05 -0400

    Refactor using redue

--- random/longest.js
+++ random/longest.js
@@ -1,12 +1,19 @@
 // Write a function that takes an array of strings as argument
 // Return the longest string
 
-const myFunction = (arr) => {
-  let longest = '';
-  arr.forEach(el => {
-    longest = el.length > longest.length ? el : longest;
-  })
-  return longest;
+// const myFunction = (arr) => {
+//   let longest = '';
+//   arr.forEach(el => {
+//     longest = el.length > longest.length ? el : longest;
+//   })
+//   return longest;
+// }
+
+//Refactor
+const myFunction = arr => {
+  return arr.reduce((acc, cur) => {
+    return acc.length > cur.length ? acc : cur;
+  }, '')
 }
 
 console.log(myFunction(['help', 'me'])); //help
